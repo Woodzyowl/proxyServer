129b4a58006909f080c23b2c285c63fb6c1dd673
John Hyatt <hyattjp0@sewanee.edu>
2016-04-17 15:13:18 -0500

    Put echo server code into proxy.c

---
+++ proxyserver/proxy.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+/* You won't lose style points for including this long line in your code */
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+
+
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include "csapp.h"
+
+/* Destructively modify string to be upper case */
+void upper_case(char *s)
+{
+  while (*s) {
+    *s = toupper(*s);
+    s++;
+  }
+}
+
+void echo(int connfd) 
+{
+    size_t n; 
+    char buf[MAXLINE]; 
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+	printf("server received %d bytes\ndata: %s", (int) n, buf);
+	upper_case(buf);
+	Rio_writen(connfd, buf, n);
+    }
+}
+
+int main(int argc, char **argv) 
+{
+    int listenfd, connfd, port;
+    socklen_t clientlen;
+    struct sockaddr_in clientaddr;
+    struct hostent *hp;
+    char *haddrp;
+    unsigned short client_port;
+    if (argc != 2) {
+	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	exit(0);
+    }
+    port = atoi(argv[1]);
+
+    listenfd = Open_listenfd(port);
+    while (1) {
+	clientlen = sizeof(clientaddr);
+	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+	/* determine the domain name and IP address of the client */
+	hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+		sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+	haddrp = inet_ntoa(clientaddr.sin_addr);
+	client_port = ntohs(clientaddr.sin_port);
+	printf("server connected to %s (%s), network port %u\n",
+	       hp->h_name, haddrp, clientaddr.sin_port);
+	printf("server connected to %s (%s), local port %u\n",
+               hp->h_name, haddrp, client_port);
+  	echo(connfd);
+	printf("Connection closed\n");
+	Close(connfd);
+        exit(0);
+    }
+}
+/* $end echoserverimain */
