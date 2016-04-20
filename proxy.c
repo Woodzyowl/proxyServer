83133e3c0e5894573efb2f2ada17e3287b3c70a7
John Hyatt <hyattjp0@sewanee.edu>
2016-04-19 19:32:15 -0500

    Added threading and tiny code

--- proxy.c
+++ proxy.c
@@ -13,6 +13,11 @@
 #include <unistd.h>
 #include "csapp.h"
 
+void* doit(void* fd);
+void read_requesthdrs(rio_t *rp);
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg);
+
 /* Destructively modify string to be upper case */
 void upper_case(char *s)
 {
@@ -22,28 +27,118 @@ void upper_case(char *s)
   }
 }
 
-void echo(int connfd) 
+void* proxy(void* connfdp) 
 {
     size_t n; 
     char buf[MAXLINE]; 
     rio_t rio;
 
-    Rio_readinitb(&rio, connfd);
-    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+    rio_readinitb(&rio, *((int *) connfdp));
+    while((n = rio_readlineb(&rio, buf, MAXLINE)) != 0) {
 	printf("server received %d bytes\ndata: %s", (int) n, buf);
 	upper_case(buf);
-	Rio_writen(connfd, buf, n);
+	rio_writen(*((int *) connfdp), buf, n);
+    }
+    printf("Connection closed\n");
+    Close(*((int *) connfdp));
+
+    return NULL;
+}
+
+/*
+ * doit - handle one HTTP request/response transaction
+ */
+/* $begin doit */
+void* doit(void* fd) 
+{
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE];
+    rio_t rio;
+
+    /* Read request line and headers */
+    rio_readinitb(&rio, *((int *)fd));
+    if (!rio_readlineb(&rio, buf, MAXLINE))  //line:netp:doit:readrequest
+        return NULL;
+    printf("%s", buf);
+    sscanf(buf, "%s %s %s", method, uri, version);       //line:netp:doit:parserequest
+    if (strcasecmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
+        clienterror(*((int *)fd), method, "501", "Not Implemented",
+                    "Tiny does not implement this method");
+        return NULL;
+    }                                                    //line:netp:doit:endrequesterr
+    read_requesthdrs(&rio);                              //line:netp:doit:readrequesthdrs
+
+    if (stat(filename, &sbuf) < 0) {                     //line:netp:doit:beginnotfound
+	clienterror(*((int *)fd), filename, "404", "Not found",
+		    "Tiny couldn't find this file");
+	return NULL;
+    }                                                    //line:netp:doit:endnotfound
+
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //line:netp:doit:readable
+        clienterror(*((int *)fd), filename, "403", "Forbidden",
+		"Tiny couldn't read the file");
+        return NULL;
+    }
+    return NULL;
+    //serve_static(fd, filename, sbuf.st_size);        //line:netp:doit:servestatic
+}
+/* $end doit */
+
+/*
+ * clienterror - returns an error message to the client
+ */
+/* $begin clienterror */
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg) 
+{
+    char buf[MAXLINE], body[MAXBUF];
+
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+    /* Print the HTTP response */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    rio_writen(fd, buf, strlen(buf));
+    rio_writen(fd, body, strlen(body));
+}
+/* $end clienterror */
+
+/*
+ * read_requesthdrs - read HTTP request headers
+ */
+/* $begin read_requesthdrs */
+void read_requesthdrs(rio_t *rp) 
+{
+    char buf[MAXLINE];
+
+    rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+    while(strcmp(buf, "\r\n")) {          //line:netp:readhdrs:checkterm
+	rio_readlineb(rp, buf, MAXLINE);
+	printf("%s", buf);
     }
 }
+/* $end read_requesthdrs */
 
 int main(int argc, char **argv) 
 {
-    int listenfd, connfd;
+    int listenfd;
+    int *connfdp;
     socklen_t clientlen;
     struct sockaddr_in clientaddr;
     struct hostent *hp;
     char *haddrp;
     unsigned short client_port;
+    pthread_t *tid=(pthread_t *)malloc(sizeof(pthread_t));
     if (argc != 2) {
 	fprintf(stderr, "usage: %s <port>\n", argv[0]);
 	exit(0);
@@ -52,7 +147,8 @@ int main(int argc, char **argv)
     listenfd = Open_listenfd(argv[1]);
     while (1) {
 	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        connfdp=(int *) malloc(sizeof(int));
+	*connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 	/* determine the domain name and IP address of the client */
 	hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
 		sizeof(clientaddr.sin_addr.s_addr), AF_INET);
@@ -62,10 +158,8 @@ int main(int argc, char **argv)
 	       hp->h_name, haddrp, clientaddr.sin_port);
 	printf("server connected to %s (%s), local port %u\n",
                hp->h_name, haddrp, client_port);
-  	echo(connfd);
-	printf("Connection closed\n");
-	Close(connfd);
-        exit(0);
+        pthread_create(tid,NULL,doit,connfdp);
     }
+    exit(0);
 }
 /* $end echoserverimain */
